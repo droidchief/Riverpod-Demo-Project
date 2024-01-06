6b81472540d6b4d0f1d98aa117acb5f68a9e46b6
Vicviral <lovedayv7@gmail.com>
2024-01-06 21:04:15 +0100

    create user view model provider

---
+++ lib/domain/usecase/provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_demo_project/domain/usecase/user_usecase.dart';
+
+import '../../infrastructure/repository/provider.dart';
+
+final usersListProvider = Provider<UserUseCase>((ref){
+  return UserUseCaseImpl(ref.read(userListProvider));
+});
+
+final createUserProvider = Provider<UserUseCase>((ref){
+  return UserUseCaseImpl(ref.read(addNewUserProvider));
+});
+
+final updateUserDataProvider = Provider<UserUseCase>((ref){
+  return UserUseCaseImpl(ref.read(updateUserProvider));
+});
+
+final deleteUserDataProvider = Provider<UserUseCase>((ref){
+  return UserUseCaseImpl(ref.read(deleteUserProvider));
+});
